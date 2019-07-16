191ac1042eb4da1f6c76d138cfeab0085852703b
Bill Kremer <bill.kremer.dev@gmail.com>
2019-07-15 22:20:56 -0500

    updated migrations for tables.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,32 +1,35 @@
 <?php
+// this isn't needed unless authentication or accounts are used - BK 15 July 2019
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
-{
+// 
+// use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Database\Migrations\Migration;
+// 
+// class CreatePasswordResetsTable extends Migration
+// {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-    }
+//     public function up()
+//     {
+//         Schema::create('password_resets', function (Blueprint $table) {
+//             $table->string('email')->index();
+//             $table->string('token');
+//             $table->timestamp('created_at')->nullable();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('password_resets');
-    }
-}
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         Schema::dropIfExists('password_resets');
+//     }
+// }
