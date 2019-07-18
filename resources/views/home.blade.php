ed64c2561cb3b71724618d3253b2c6b01260576f
Bill Kremer <bill.kremer.dev@gmail.com>
2019-07-17 21:59:52 -0500

    work on formatting first page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,30 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-left">
-        <h3 class="">Discover Your Perspective</h3>
-        <h3 class="">Complete the 7 minute test and get a detailed report of your lenses on the world. 
+        <p class=""><strong>Discover Your Perspective</strong>
+        <br>
+        <span class="text-muted">Complete the 7 minute test and get a detailed report of your lenses on the world.</p>
     </div>
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="row">
-                <form >
-
+                <form method="GET" action="{{ route('results') }}">
+                    @csrf
 
                 @foreach ($questions as $question) 
                     @include('question', $question)
                 @endforeach
 
 
-                <div class="light-outline">
-                    <label>
-                    <h3>Your Email</h3>
-                    <input type="email" required="required" autocomplete="email" placeholder="you@example.com">
+                <div class="light-outline form-group">
+                    <label class="question-text">Your Email</label>
+                    <input type="email" required="required" autocomplete="email" name="email" class="form-control form-control-lg" placeholder="you@example.com">
 
 
 
                 </div>
 
-                <button type="submit" class="">Save &amp; Continue</button> 
+                <button type="submit" class="btn btn-primary text-center">Save &amp; Continue</button> 
 
 
                 </form>
--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,37 +1,39 @@
  {{-- this is a blade to display the individual questions --}}
 
- <div class="light-outline">
-    <h3>{{$question}}</h3>
-    <div>
+{{-- TODO add aria-label=... --}}
+{{-- TODO make all inputs required & add error? is input error enough? --}}
+ <div class="light-outline text-center">
+    <p class="question-text">{{$question}}</p>
+    <div class="radio-stretch">
       <span class="text-danger">Disagree</span>
 
       <input type="radio" id="question{{$id}}Answer1"
-          name="questionAnswer" value="1">
-      <label for="question{{$id}}Answer1" hidden="hidden">1</label>
+          name="questionAnswer-{{$id}}" value="1" required="required">
+      <label for="question{{$id}}Answer1" class="sr-only sr-only-focusable">1</label>
 
       <input type="radio" id="question{{$id}}Answer2"
-          name="questionAnswer" value="2">
-      <label for="question{{$id}}Answer2" hidden="hidden">2</label>
+          name="questionAnswer-{{$id}}" value="2" required="required">
+      <label for="question{{$id}}Answer2" class="sr-only sr-only-focusable">2</label>
 
       <input type="radio" id="question{{$id}}Answer3"
-          name="questionAnswer" value="3">
-      <label for="question{{$id}}Answer3" hidden="hidden">3</label>
+          name="questionAnswer-{{$id}}" value="3" required="required">
+      <label for="question{{$id}}Answer3" class="sr-only sr-only-focusable">3</label>
 
       <input type="radio" id="question{{$id}}Answer4"
-          name="questionAnswer" value="4">
-      <label for="question{{$id}}Answer4" hidden="hidden">4</label>
+          name="questionAnswer-{{$id}}" value="4">
+      <label for="question{{$id}}Answer4" class="sr-only sr-only-focusable">4</label>
 
       <input type="radio" id="question{{$id}}Answer5"
-          name="questionAnswer" value="5">
-      <label for="question{{$id}}Answer5" hidden="hidden">5</label>
+          name="questionAnswer-{{$id}}" value="5">
+      <label for="question{{$id}}Answer5" class="sr-only sr-only-focusable">5</label>
 
       <input type="radio" id="question{{$id}}Answer6"
-          name="questionAnswer" value="6">
-      <label for="question{{$id}}Answer6" hidden="hidden">6</label>
+          name="questionAnswer-{{$id}}" value="6">
+      <label for="question{{$id}}Answer6" class="sr-only sr-only-focusable">6</label>
 
       <input type="radio" id="question{{$id}}Answer7"
-          name="questionAnswer" value="7">
-      <label for="question{{$id}}Answer7" hidden="hidden">7</label>
+          name="questionAnswer-{{$id}}" value="7">
+      <label for="question{{$id}}Answer7" class="sr-only sr-only-focusable">7</label>
 
       <span class="text-success">Agree</span>
     </div>
