f12a3429785d73676fee31f47378e690fddf51d4
Bill Kremer <bill.kremer.dev@gmail.com>
2019-07-19 13:36:22 -0500

    added basic info to Quiz model

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    //
-}
+/**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'quizzes';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'quizname',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 
+    ];}
