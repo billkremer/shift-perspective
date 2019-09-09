1de56210f41066aeffb7f77c1cea952b181f8cab
Bill Kremer <bill.kremer.dev@gmail.com>
2019-09-09 02:44:52 -0500

    updated js link to redirect to https instead of just http

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js', env('REDIRECT_HTTPS')) }}" defer></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css', env('REDIRECT_HTTPS')) }}" rel="stylesheet">
